3468bf06f8c4771e7ba9be002611ca4c28fc0939
Christophe Gondouin <14264672+cgoconseils@users.noreply.github.com>
2025-01-17 12:06:55 +0100

    Removing dependency to Microsoft.Xrm.Sdk

--- src/Azure.Functions.Worker.Extensions.MediatR/OpenApi/Extensions/TypeExtensions.cs
+++ src/Azure.Functions.Worker.Extensions.MediatR/OpenApi/Extensions/TypeExtensions.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.Xrm.Sdk;
-
-namespace Azure.Functions.Worker.Extensions.MediatR.OpenApi.Extensions;
+﻿namespace Azure.Functions.Worker.Extensions.MediatR.OpenApi.Extensions;
 
 public static class TypeExtensions
 {
+    private static readonly Lazy<Type?> DataCollection1Type = new(() => AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.FullName == "Microsoft.Xrm.Sdk.DataCollection`1"));
+    private static readonly Lazy<Type?> DataCollection2Type = new(() => AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.FullName == "Microsoft.Xrm.Sdk.DataCollection`2"));
+    
+    private static Type? DataCollection1TypeValue => DataCollection1Type.Value;
+    private static Type? DataCollection2TypeValue => DataCollection2Type.Value;
+    
     public static bool IsDataCollectionType(this Type type)
-        => (type.BaseType?.IsGenericType ?? false) && type.IsAssignableTo(type.GetDataCollectionType());
+        => DataCollection1TypeValue != null && DataCollection2TypeValue != null && (type.BaseType?.IsGenericType ?? false) && type.IsAssignableTo(type.GetDataCollectionType());
 
-    public static Type GetDataCollectionType(this Type type)
+    private static Type GetDataCollectionType(this Type type)
         => type.BaseType?.GenericTypeArguments.Length == 2
-            ? typeof(DataCollection<,>).MakeGenericType(type.BaseType.GenericTypeArguments)
-            : typeof(DataCollection<>).MakeGenericType(type.BaseType!.GenericTypeArguments);
+            ? DataCollection2TypeValue!.MakeGenericType(type.BaseType.GenericTypeArguments)
+            : DataCollection1TypeValue!.MakeGenericType(type.BaseType!.GenericTypeArguments);
 }
